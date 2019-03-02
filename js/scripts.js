14054738cdbbd3963e253b24768b22f9d0907ac7
Rees Alumasa <reesalumasa@gmail.com>
2019-03-02 13:50:43 +0300

    Add modifications to indes, css and js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,106 @@
-function Dice(side) {
-  this.play1=side;
-  this.play2=side;
-  this.play=function () {
-    return Math.floor(Math.random() * this.play1) + 1;
-    return Math.floor(Math.random() * this.play2) + 1;
+var players = []
+var switchArray = []
+
+function Contest(names, score) {
+  this.name= names;
+  this.score = score;
+  players.push(this);
+}
+
+Array.prototype.sum = function() {
+  return this.reduce(function(a,b) {return a + b});
+}
+
+// UI logic
+$(document).ready(function() {
+  // Player name submit button
+  $("form#cont").submit(function(event) {
+    event.preventDefault();
+    // debugger;
+    var name1 = $("#name1").val();
+    var name2 = $("#name2").val();
+    var player1 = new Contest(name1, 0);
+    var player2 = new Contest(name2, 0);
+    $("#pshow1").text(players[0].name + "'s turn").show();
+    contestShow();
+    $("form").hide();
+    $(".play").show();
+  });
+
+  $("#roll").click(function(event) {
+    event.preventDefault();
+    $("#result").show();
+    $(".turnTotal").show();
+    var random= (Math.floor(Math.random() * 6)+1);
+    $("#result").text(random);
+    if (random >= 2) {
+      switchArray.push(random);
+      $("#turnTotal").text(switchArray.sum());
+    } else {
+      switchP();
+    }
+  });
+
+// player 1
+  $("#endTurn").click(function(event){
+    change();
+    if (players[0].score >= 100){
+      var win=(players[0].name + " wins!!");
+      $("#win").text(win)
+      document.location.reload(true)
+    } else if (players[1].score >= 100) {
+      $("#win").text(players[1].name + " wins!!");
+      document.location.reload(true);
   }
-};
+  });
 
-function Player(score) {
-  var player1=document.getElementById('player1')
-  player1.innerHTML = score;
-};
+  function contestShow() {
+    $("#name1").text(players[0].name);
+    $("#name2").text(players[1].name);
+    $("#total1").text(players[0].score);
+    $("#total2").text(players[1].score);
+  }
 
-function Player2(score) {
-  var player1=document.getElementById('player1')
-  player2.innerHTML = score;
-};
+  function switchP() {
+      if ($("#pshow1").is(":visible")) {
+        switchArray = [0];
+        $("#turnTotal").text(switchArray);
+        setTimeout(function() { $("#roll1").text("Oops! You rolled 1"); }, 50 );
+        $("#roll1").hide();
+        setTimeout(function() {$("#pshow2").text(players[1].name + "'s turn").show();}, 100 );
+        $("#pshow1").hide();
+        contestShow();
+          }
+      else if ($("#pshow2").is(":visible")){
+        switchArray = [0];
+        $("#turnTotal").text(switchArray);
+        setTimeout(function() { $("#roll1").text("Oops! You rolled 1"); }, 50 );
+        setTimeout(function() {$("#pshow1").text(players[0].name + "'s turn").show();}, 100 );
+        $("#roll1").show();
+        $("#pshow2").hide();
+        contestShow();
+      }
+    }
 
-var scored = new Dice(6);
-
-var button =document.getElementById('button')
-button.onclick = function() {
-  var result = scored.play();
-  var result2 = scored.play();
-  Player("Score: "+result)
-  Player2("Score: "+result2)
-}
-function Total(pl) {
-  var showTotal=document.getElementById('score')
-  showTotal.innerHTML = pl;
-};
-var hold = document.getElementById("hold")
-hold.onClick = function() {
-  var result= result;
-  alert(Total(result))
-}
+  function change() {
+    if ($("#pshow1").is(":visible")) {
+      players[0].score = (players[0].score += switchArray.sum());
+      alert(players[0].name + ", you got " + switchArray.sum() +  " points!");
+      switchArray = [0];
+      $("#turnTotal").text(switchArray)
+      $("#pshow1").hide();
+      $("#pshow2").text(players[1].name + "'s turn").show();
+      console.log(players[0].score);
+      contestShow();
+    } else {
+      alert(players[1].name + ", you got " + switchArray.sum() +  " points!");
+      players[1].score = (players[1].score += switchArray.sum());
+      switchArray = [0];
+      $("#turnTotal").text(switchArray)
+      $("#pshow1").hide();
+      $("#pshow2").text(players[0].name + "'s turn").show();
+      contestShow();
+      console.log(players[1].score);
+      }
+    }
+});
